84cc01f8f86d4c30e3039caa4dd1bd7caed09d75
chenanqi1024 <chenanqi1024@gmail.com>
2026-07-13 00:02:56 +0800

    Initial Commit

---
+++ AIPlan/AIPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIPlanApp.swift
+//  AIPlan
+//
+//  Created by chenanqi on 2026/7/13.
+//
+
+import SwiftUI
+
+@main
+struct AIPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIPlan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AIPlan
+//
+//  Created by chenanqi on 2026/7/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
